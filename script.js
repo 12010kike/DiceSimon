072d97e51985cb569447d2005c370ce5c52fabe3
enriqueleonn <ealu2005@gmail.com>
2025-02-15 19:17:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let secuencia = [];
 let secuenciaJugador = [];
 let puntaje = 0;
 let nombreJugador = "";
+
+document.getElementById("iniciarJuego").addEventListener("click", () => {
+    nombreJugador = document.getElementById("nombreJugador").value;
+    if (nombreJugador === "") {
+    alert("Ingresa tu nombre para empezar.");
+    return;
+    }
+    document.getElementById("jugadorActual").textContent = nombreJugador;
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("juego").style.display = "block";
+    reiniciarJuego();
+    nuevaRonda();
+    });
